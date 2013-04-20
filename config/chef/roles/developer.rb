fddaa34440c0fac84f17e2eb80c1803103e68293
Chaitanya Sharma <gopi.daiict@gmail.com>
2013-04-20 20:29:09 +0530

    added java cookbook

--- config/chef/roles/developer.rb
+++ config/chef/roles/developer.rb
@@ -6,6 +6,7 @@ run_list %w(
 	recipe[git]
 	recipe[vim]
 	recipe[emacs]
+        recipe[java]
 	recipe[apache2]
 	recipe[mysql::server]
 	recipe[build-essential]
@@ -17,6 +18,12 @@ default_attributes(
 		:server_repl_password => "pass-1-5-6-bar",
 		:server_debian_password => "pass-1-5-6-hoo"
 	},
+        :java => {
+                :install_flavor => "oracle",
+                :oracle => {
+                       "accept_oracle_download_terms" => true
+                }
+        },
 	:authorization => {
 		:sudo => {
 			:users => %w(csharma developer ubuntu),
