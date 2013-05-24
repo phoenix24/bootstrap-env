30a180b1e5f4ffbde04b88b0e5ec649384af2bed
Chaitanya Sharma <gopi.daiict@gmail.com>
2013-05-24 08:22:25 +0530

    added java + maven, and few more dev utils.

--- config/chef/roles/developer.rb
+++ config/chef/roles/developer.rb
@@ -4,11 +4,22 @@ description "Role for combined Development Box, used for Developer VMs"
 run_list %w(
  role[base]
  recipe[apt]
+ recipe[htop]
+ recipe[curl]
+ recipe[emacs]
+ recipe[mysql]
+
+ recipe[java]
+ recipe[build-essential]
+
+ recipe[git]
+ recipe[subversion]
+
+ recipe[maven]
 )
 
 default_attributes(
  :java => {
-   :install_flavor => "oracle",
    :oracle => {
      "accept_oracle_download_terms" => true
    }
@@ -19,6 +30,10 @@ default_attributes(
      :groups => %w(admin wheel sysadmin),
      :passwordless => true
    }
+ },
+ :maven => {
+   :version => 3,
+   :steup_bin => true
  }
 )
 override_attributes(
